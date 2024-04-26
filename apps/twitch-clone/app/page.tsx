c0ceadbf97f39a390c9bbdd2a82eea98ea9a6d3d
igiza1213 <igiza1213@gmail.com>
2024-04-26 12:25:16 +0900

    init

--- apps/twitch-clone/app/page.tsx
+++ apps/twitch-clone/app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <p>Hello, Twitch</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
